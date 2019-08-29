a9a7a915a2f6cfd7d1bd1d986e73445888fd6260
Isaac-Hatfield <ikehat01@gmail.com>
2019-08-29 08:40:04 -0500

    Update hello.c
    added required code

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+ *Isaac Hatfield
+ *29 August 2019
+ *a short name printing type program
+ */
+#include <stdio.h>
+
+int main()
+{
+  printf("Isaac Hatfield\n");
+  printf("Major:Math/Computer Science\n")
+  return 0;
+}
